8ff7e024d8f841dea1b8d4600c6b702384b43b0f
bitbitbot <39787833+bitbitbot@users.noreply.github.com>
2022-03-11 23:26:39 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,11 +1,8 @@
-#def foo(a):
-#    return a+2
-#^old
 def increment_by_two(var_to_increment):
-       """
-       This function increments input by two
-       :param var_to_increment: param to increment
-       :return: incrememented variable
-       """
-       return var_to_increment + 2
+    """
+    This function increments input by two
+    :param var_to_increment: param to increment
+    :return: incrememented variable
+    """
+    return var_to_increment + 2
     
