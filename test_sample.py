8b9fea6b64497193a80cd9e62deb6c3a6e1b429f
bitbitbot <39787833+bitbitbot@users.noreply.github.com>
2022-03-11 23:36:21 -0500

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -11,11 +11,11 @@ class TestSampleMethods(unittest.TestCase):
     """
     
     def test_incremement_by_two(self):
-      """
-      Test incremements
-      """
-      self.assertEqual(increment_by_two(-2),0)
-      self.assertEqual(increment_by_two(0),2)
-      self.assertEqual(increment_by_two(3),5)
+        """
+        Test incremements
+        """
+        self.assertEqual(increment_by_two(-2),0)
+        self.assertEqual(increment_by_two(0),2)
+        self.assertEqual(increment_by_two(3),5)
 if __name__ == '__main__':
     unittest.main()
